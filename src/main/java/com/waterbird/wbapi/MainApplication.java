c1d881d521e1f04635f513d36f1ed58cf8fff087
liancong <lcc1105897090@126.com>
2024-01-01 04:16:17 +0800

    Nacos整合示例

--- src/main/java/com/waterbird/wbapi/MainApplication.java
+++ src/main/java/com/waterbird/wbapi/MainApplication.java
@@ -1,5 +1,6 @@
 package com.waterbird.wbapi;
 
+import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -17,6 +18,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @MapperScan("com.waterbird.wbapi.mapper")
 @EnableScheduling
 @EnableAspectJAutoProxy(proxyTargetClass = true, exposeProxy = true)
+@EnableDubbo
 public class MainApplication {
 
     public static void main(String[] args) {
